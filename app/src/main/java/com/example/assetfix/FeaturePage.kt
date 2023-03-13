a715bd9bbdcf5ac056c46c7ee41b9659542d97e0
smuchara <68949651+smuchara@users.noreply.github.com>
2023-03-13 11:27:45 +0300

    Added transition between layouts

---
+++ app/src/main/java/com/example/assetfix/FeaturePage.kt
@@ -0,0 +1,11 @@
+package com.example.assetfix
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FeaturePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.feature_page)
+    }
+}
