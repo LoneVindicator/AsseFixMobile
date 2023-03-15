ebf081c93db1f864fc9fc8809be2003f904ac906
smuchara <68949651+smuchara@users.noreply.github.com>
2023-03-15 13:15:43 +0300

    Implemented the log in page and added input validation logic,Worked on navigation between multiple pages

---
+++ app/src/main/java/com/example/assetfix/mobile/dashboard/Dashboard.kt
@@ -0,0 +1,9 @@
+package com.example.assetfix.mobile.dashboard
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.assetfix.R.layout.dashboard)
+    }
+}
--- app/src/main/java/com/example/assetfix/mobile/welcomepages/FeaturePage.kt
+++ app/src/main/java/com/example/assetfix/mobile/welcomepages/FeaturePage.kt
@@ -1,12 +1,24 @@
 package com.example.assetfix.mobile.welcomepages
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.assetfix.R
+import com.example.assetfix.mobile.auth.LogInpage
 
 class FeaturePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.feature_page)
+        var logInButton: Button = findViewById(R.id.login)
+        logInButton.setOnClickListener {
+            openLogInPage()
+        }
+    }
+    private fun openLogInPage() {
+        val intent = Intent(this, LogInpage::class.java)
+        startActivity(intent)
+        overridePendingTransition(R.anim.slide_to_the_right, R.anim.slide_to_the_left)
     }
 }
