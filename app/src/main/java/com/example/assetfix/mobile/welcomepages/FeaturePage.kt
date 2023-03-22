12339c0dc909551bc4aea01d0a49bc9acb2ad1d5
smuchara <68949651+smuchara@users.noreply.github.com>
2023-03-22 16:03:49 +0300

    Added the side navigation menu

--- app/src/main/java/com/example/assetfix/mobile/welcomepages/FeaturePage.kt
+++ app/src/main/java/com/example/assetfix/mobile/welcomepages/FeaturePage.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.assetfix.R
 import com.example.assetfix.mobile.auth.LogInpage
+import com.example.assetfix.mobile.auth.SignUpPage
+import com.google.android.material.button.MaterialButton
 
 class FeaturePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +17,18 @@ class FeaturePage : AppCompatActivity() {
         logInButton.setOnClickListener {
             openLogInPage()
         }
+        var signUpButton: MaterialButton = findViewById(R.id.signup)
+        signUpButton.setOnClickListener {
+            openSignUpPage()
+        }
     }
+
+    private fun openSignUpPage() {
+        val intent = Intent(this, SignUpPage::class.java)
+        startActivity(intent)
+        overridePendingTransition(R.anim.slide_to_the_right, R.anim.slide_to_the_left)
+    }
+
     private fun openLogInPage() {
         val intent = Intent(this, LogInpage::class.java)
         startActivity(intent)
