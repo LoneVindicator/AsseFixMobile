4ab9d2d5c5a760c1e767bbbcf7404d9d86545d24
LoneVindicator <68654547+LoneVindicator@users.noreply.github.com>
2024-01-25 09:49:06 +0300

    created fragment for each page & its appropriate navigation

--- app/src/main/java/com/example/assetfix/mobile/main/MainActivity.kt
+++ app/src/main/java/com/example/assetfix/mobile/main/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.example.assetfix.mobile.main
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import com.example.assetfix.R
+import com.example.assetfix.mobile.assets.AssetsFragment
 import com.example.assetfix.mobile.dashboard.DashboardFragment
-import com.example.assetfix.mobile.dashboard.adapter.ItemAdapter
-import com.example.assetfix.mobile.dashboard.data.Datasource
+import com.example.assetfix.mobile.inspectionsandsupervision.InspectionsAndSupervisionFragment
+import com.example.assetfix.mobile.partsandsupplies.PartsAndSuppliesFragment
+import com.example.assetfix.mobile.peopleandteams.PeopleAndTeamsFragment
+import com.example.assetfix.mobile.profile.ProfileFragment
+import com.example.assetfix.mobile.vendorsandcustomers.VendorsAndCustomersFragment
 import com.example.assetfix.mobile.workOrder.WorkOrderFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -47,25 +52,33 @@ class MainActivity : AppCompatActivity() {
                     replaceFragment(DashboardFragment(), toolbarTitle)
                 }
                 R.id.side_menu_profile -> {
-                    Toast.makeText(applicationContext, "Clicked Profile", Toast.LENGTH_SHORT).show()
+                    toolbarTitle = "Profile"
+                    replaceFragment(ProfileFragment(), toolbarTitle)
                 }
                 R.id.side_menu_assets -> {
-                    Toast.makeText(applicationContext, "Clicked Assets", Toast.LENGTH_SHORT).show()
+                    toolbarTitle = "Assets"
+                    replaceFragment(AssetsFragment(), toolbarTitle)
                 }
                 R.id.side_menu_parts_and_supplies -> {
-                    Toast.makeText(applicationContext, "Clicked Parts & Supplies", Toast.LENGTH_SHORT).show()
+                    toolbarTitle = "Parts & Supplies"
+                    replaceFragment(PartsAndSuppliesFragment(), toolbarTitle)
                 }
                 R.id.side_menu_people_and_teams -> {
-                    Toast.makeText(applicationContext, "Clicked Peoples & Teams", Toast.LENGTH_SHORT).show()
+                    toolbarTitle = "People & Teams"
+                    replaceFragment(PeopleAndTeamsFragment(), toolbarTitle)
                 }
                 R.id.side_menu_inspections_and_supervision ->{
-                    Toast.makeText(applicationContext, "Inspections & Supervision", Toast.LENGTH_SHORT).show()
+                    toolbarTitle = "Inspections & Supervision"
+                    replaceFragment(InspectionsAndSupervisionFragment(), toolbarTitle)
                 }
                 R.id.side_menu_vendors_and_customers ->{
-                    Toast.makeText(applicationContext, "Clicked Vendors & Customers", Toast.LENGTH_SHORT).show()
+                    toolbarTitle = "Vendors & Customers"
+                    replaceFragment(VendorsAndCustomersFragment(), toolbarTitle)
                 }
                 R.id.side_menu_sign_out ->{
                     Toast.makeText(applicationContext, "Clicked Sign Out", Toast.LENGTH_SHORT).show()
+                    toolbarTitle = "Home"
+                    replaceFragment(DashboardFragment(), toolbarTitle)
                 }
             }
             // Close the drawer after a menu item is selected
@@ -89,12 +102,12 @@ class MainActivity : AppCompatActivity() {
                 R.id.bottom_nav_menu_assets_item -> {
 
                     toolbarTitle = "Assets"
-                    replaceFragment(DashboardFragment(), toolbarTitle)
+                    replaceFragment(AssetsFragment(), toolbarTitle)
                     return@setOnItemSelectedListener true
                 }
                 R.id.bottom_nav_menu_people_and_teams_item -> {
                     toolbarTitle = "People & Teams"
-                    replaceFragment(DashboardFragment(), toolbarTitle)
+                    replaceFragment(PeopleAndTeamsFragment(), toolbarTitle)
                     return@setOnItemSelectedListener true
                 }
 
@@ -103,6 +116,14 @@ class MainActivity : AppCompatActivity() {
                 else -> return@setOnItemSelectedListener false
             }
         }
+
+        val qrScanBtn: FloatingActionButton = findViewById(R.id.qrScanView)
+
+        // Now you can interact with the FloatingActionButton
+        qrScanBtn.setOnClickListener {
+            Toast.makeText(this, "QR Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 
