9e4a6ff19349fb3c8e784d72d21ce46249012581
LoneVindicator <68654547+LoneVindicator@users.noreply.github.com>
2024-02-01 13:02:34 +0300

    Got a response from the server

--- app/src/main/java/com/example/assetfix/mobile/workOrder/WorkOrderFragment.kt
+++ app/src/main/java/com/example/assetfix/mobile/workOrder/WorkOrderFragment.kt
@@ -2,6 +2,7 @@ package com.example.assetfix.mobile.workOrder
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +16,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.assetfix.R
 import com.example.assetfix.mobile.workOrder.adapter.ItemAdapter
 import com.example.assetfix.mobile.workOrder.data.Datasource
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Header
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -34,6 +42,9 @@ class WorkOrderFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: ItemAdapter // replace MyAdapter with your actual adapter class
+
+    private val baseUrl = "https://test.assetfix.co/api/"
+    private lateinit var apiService: ApiService
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -102,6 +113,15 @@ class WorkOrderFragment : Fragment() {
                     // If not empty, make the LinearLayout gone
                     emptyLayout.visibility = View.GONE
                 }
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        apiService = retrofit.create(ApiService::class.java)
+
+        fetchData()
     }
 
     private fun changeActivityTitle(newTitle: String) {
@@ -123,6 +143,79 @@ class WorkOrderFragment : Fragment() {
         startActivity(intent)
 
     }
+    interface ApiService {
+        @GET("work-orders")
+        fun getData(@Header("Authorization") token: String): Call<MaintenanceData>
+    }
+
+    private fun fetchData() {
+        val accessToken = "30|028dowtjgcLF9WFHbZy84OtpsANgw8HF8UNptMli"
+
+        val call = apiService.getData("Bearer $accessToken")
+        call.enqueue(object : Callback<MaintenanceData> {
+            override fun onResponse(call: Call<MaintenanceData>, response: Response<MaintenanceData>) {
+                if (response.isSuccessful) {
+                    val data = response.body()
+                    // Process the data as needed
+                    logData(data)
+                } else {
+                    // Handle error
+                    handleErrorResponse(response)
+                }
+            }
+
+            override fun onFailure(call: Call<MaintenanceData>, t: Throwable) {
+                // Handle failure
+                Log.e("ApiCall", "API call failed", t)
+            }
+        })
+    }
+
+    private fun logData(data: MaintenanceData?) {
+        if (data != null) {
+            // Log the data here
+            Log.d("ApiCall", "Data received: $data")
+        } else {
+            Log.w("ApiCall", "Data is null")
+        }
+    }
+
+    private fun handleErrorResponse(response: Response<MaintenanceData>) {
+        // Log the error details
+        Log.e("ApiCall", "Error: ${response.code()}, ${response.message()}")
+        // You can also log the error body if needed: Log.e("ApiCall", "Error Body: ${response.errorBody()?.string()}")
+    }
+
+    data class MaintenanceData(
+        val data: List<MaintenanceItem>
+    )
+
+    data class MaintenanceItem(
+        val id: Int,
+        val assetableType: String?,
+        val assetableId: Int?,
+        // ... other properties
+        val assetable: Assetable?,
+        val requestor: Requestor?
+    )
+
+    data class Assetable(
+        val id: Int,
+        val name: String,
+        val address: String,
+        val city: String,
+        val country: String,
+        // ... other properties
+    )
+
+    data class Requestor(
+        val id: Int,
+        val name: String,
+        val email: String,
+        val phoneNumber: String
+        // ... other properties
+    )
+
 
     companion object {
         /**
