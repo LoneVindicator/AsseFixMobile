d062f1f3a89b2311f491b0ac6767270900af8741
LoneVindicator <68654547+LoneVindicator@users.noreply.github.com>
2024-02-05 13:29:13 +0300

    implemented partial form validation

--- app/src/main/java/com/example/assetfix/mobile/workOrder/WorkOrderFormFragment.kt
+++ app/src/main/java/com/example/assetfix/mobile/workOrder/WorkOrderFormFragment.kt
@@ -1,11 +1,19 @@
 package com.example.assetfix.mobile.workOrder
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 import com.example.assetfix.R
+import com.google.android.material.button.MaterialButton
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,6 +44,121 @@ class WorkOrderFormFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_work_order_form, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val submitBtn: MaterialButton? = view?.findViewById(R.id.create_work_order_submit_btn)
+
+        val issueSummaryTextView = view.findViewById<TextView>(R.id.issue_summary_input)
+
+        val assignedToSpinner: Spinner = view.findViewById(R.id.assigned_to_spinner)
+        val assetLocationSpinner: Spinner = view.findViewById(R.id.asset_location_spinner)
+
+        var selectedAssignedToOption = ""
+        var selectedAssetLocationOption = ""
+
+        // Set an item selected listener for the spinner
+        assignedToSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // Retrieve the selected item directly from the Spinner
+                selectedAssignedToOption = assignedToSpinner.selectedItem.toString()
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Handle the case where no item is selected
+                selectedAssignedToOption = ""
+            }
+
+        }
+
+        assetLocationSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // Retrieve the selected item directly from the Spinner
+                selectedAssetLocationOption = assetLocationSpinner.selectedItem.toString()
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Handle the case where no item is selected
+                selectedAssetLocationOption = ""
+            }
+
+        }
+
+        submitBtn?.setOnClickListener {
+
+            var isFormValid: Boolean = validateForm(issueSummaryTextView.text.toString(), selectedAssetLocationOption, selectedAssignedToOption)
+
+            if(isFormValid){
+
+//                postWorkOrder()
+                Toast.makeText(context, "Form validation successful", Toast.LENGTH_SHORT).show()
+
+
+            }else{
+
+                Toast.makeText(context, "Formation validation failed!", Toast.LENGTH_SHORT).show()
+
+
+            }
+
+
+        }
+
+
+    }
+
+    private fun postWorkOrder() {
+        TODO("Not yet implemented")
+
+    }
+
+    private fun validateForm(issueSummary: String, assetLocation: String, assignedTo: String): Boolean {
+
+        Log.d("workOrderForm", issueSummary)
+        Log.d("workOrderForm", assetLocation)
+        Log.d("workOrderForm", assignedTo)
+
+        // Check if issueSummary is not empty
+        if (issueSummary.isEmpty()) {
+            // Display an error message or handle the empty issueSummary case
+            Log.d("workOrderForm", "Issue summary is required.")
+            Toast.makeText(context, "Issue Summary CANNOT be empty", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        // Check if assetLocation is not empty
+        if (assetLocation.isEmpty() || assetLocation == "-- Select an asset or location --") {
+            // Display an error message or handle the empty or default assetLocation case
+            Log.d("workOrderForm", "Please select a valid asset location.")
+            Toast.makeText(context, "Asset/Location MUST be selected", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        // Check if assignedTo is not empty
+        if (assignedTo.isEmpty() || assignedTo == "-- Choose Assignees --") {
+            // Display an error message or handle the empty or default assignedTo case
+            Log.d("workOrderForm", "Please select a valid assignee.")
+            Toast.makeText(context, "Assignee MUST be selected", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        // All validations passed, return true
+        return true
+
     }
 
     companion object {
